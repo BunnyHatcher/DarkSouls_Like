f122508fe2264ecc557de2f3ba71812b860cb90e
BunnyHatcher <martinengelhaus@gmx.de>
2022-08-24 01:36:43 +0200

    Added StateMachine essentials
    Finished Chapter 9. Testing Player State Machine

---
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    
+    private void Start()
+    {
+       SwitchState(new PlayerTestState(this));
+    }
+
+}
