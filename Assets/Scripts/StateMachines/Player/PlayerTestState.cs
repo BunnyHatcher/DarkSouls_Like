923ada42c70d1021ec2d0dc37060b337618b05c9
BunnyHatcher <martinengelhaus@gmx.de>
2022-08-27 01:14:32 +0200

    implemented input events

--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,31 +6,33 @@ using UnityEngine;
 public class PlayerTestState : PlayerBaseState
 {
     
-    private float timer = 5f;
+    private float timer = 0;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){ }
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
+
 
     public override void Tick(float deltaTime)
     {
-       timer -= deltaTime;
+       timer += deltaTime;
 
        Debug.Log(timer);
 
-       if(timer <= 0f)
-       {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-       }
+       
 
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
     }
 
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState (stateMachine));
+    }
    
 }
