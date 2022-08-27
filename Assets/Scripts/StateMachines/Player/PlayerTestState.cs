56df7c9ad8d93e2676c9aa44d1f3e5f41589b98e
BunnyHatcher <martinengelhaus@gmx.de>
2022-08-27 13:25:46 +0200

    added character controller

--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -21,8 +21,11 @@ public class PlayerTestState : PlayerBaseState
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y; // movement on z-axis corresponds to y axis on the vector2 for movement controls
-        stateMachine.transform.Translate(movement * deltaTime);
-        
+        stateMachine.CharacterController.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if (stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
+
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
 
     }
 
